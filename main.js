a9d904a6a5baa75b19d00d666c07dd49bf070731
MohammedAlghazali <hamood-monzer11@hotmail.com>
2020-01-22 10:00:29 +0200

    add file to save relates #1

---
+++ main.js
@@ -0,0 +1,22 @@
+/*
+var  myText= document.getElementsByClassName('header-slogan').textContent,
+    i = 0;
+
+myButton.onclick = function () {
+    
+    "use strict";
+    
+    var typeWriter = setInterval(function () {
+        
+        document.getElementById('my-div').textContent += myText[i];
+        
+        i = i + 1;
+        
+        if (i > myText.length - 1) {
+            
+            clearInterval(typeWriter);
+        }
+    }, 100);
+};
+
+*/
